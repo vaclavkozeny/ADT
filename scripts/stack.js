f13eb70e97f74fb20a290467a8bfe482821e60fd
Václav Kožený <vaclav.kozeny@tul.cz>
2025-03-24 11:49:46 +0100

    animace stack, TODO: gap problém

--- scripts/stack.js
+++ scripts/stack.js
@@ -6,64 +6,145 @@ const stackSize = 5;
 var stack = [];
 var errorFullDisplayed = false;
 var errorEmptyDisplayed = false;
+var animating = false;
+var greenArrow = document.getElementById('inArrow');
+var redArrow = document.getElementById('outArrow');
+var stackElem = document.getElementById('stack');
 
-function sPush(){
-    if(stack.length < stackSize){
-        if(inputval){
-            if(stack.length != 0){
-                document.getElementById(`${stack.length}`).classList.add("hideBefore");
-                document.getElementById(`${stack.length}`).classList.add("hideAfter");
+
+document.addEventListener('DOMContentLoaded', () => {
+    gsap.set(greenArrow, { y: stackElem.getBoundingClientRect().height, opacity: 1 })
+    gsap.set(redArrow, { y: stackElem.getBoundingClientRect().height, opacity: 0 })
+})
+function stackPush() {
+    if (!animating) {
+        animating = true;
+        if (stack.length < stackSize) {
+            if (inputval) {
+                stack.push(inputval);
+                //-------- create element ------------------
+                const elem = document.createElement('div');
+                elem.textContent = inputval;
+                elem.id = stack.length;
+                elem.classList.add('data');
+                elem.classList.add('stack');
+                elem.style.order = stackSize - stack.length;
+                stackElem.appendChild(elem);
+                var pos = elem.style.top;
+                gsap.set(elem, { y: -500, opacity: 1 });
+
+                gsap.to(elem, {
+                    y: pos,
+                    duration: 0.5,
+                    ease: "power2.out",
+                });
+                if(stack.length === stackSize){
+                    gsap.to(greenArrow, {
+                        y: stackElem.getBoundingClientRect().height - (stack.length * elem.offsetHeight) - 2,
+                        duration: 0.25,
+                        opacity: 0,
+                        ease: "power2.out"
+                    });
+                }else{
+                    gsap.to(greenArrow, {
+                        y: stackElem.getBoundingClientRect().height - (stack.length * elem.offsetHeight) - 2,
+                        duration: 0.25,
+                        ease: "power2.out"
+                    });
+                }
+                
+                gsap.to(redArrow, {
+                    y: stackElem.getBoundingClientRect().height - ((stack.length - 1) * elem.offsetHeight) - 2,
+                    duration: 0.25,
+                    opacity: 1,
+                    ease: "power2.out",
+                    onComplete: () => {
+                        animating = false;
+                    }
+                });
+                animating = false;
             }
-            stack.push(inputval);
-            //-------- create element ------------------
-            const elem = document.createElement('div');
-            elem.textContent = inputval;
-            elem.id = stack.length;
-            elem.classList.add('data');
-            elem.classList.add('stack');
-            elem.style.order = stackSize-stack.length;
-            document.getElementById('stack').appendChild(elem);
         }
-    }   
-    else{
-        if(errorEmptyDisplayed === false && errorFullDisplayed === false){
-            errorFullDisplayed = true;
-            err.textContent = "Stack is full";
-            err.style.display = 'flex';
-            setTimeout(() => {
-                err.style.display = 'none';
-            }, 1000); 
-            errorFullDisplayed = false;
+
+        else {
+            if (errorEmptyDisplayed === false && errorFullDisplayed === false) {
+                errorFullDisplayed = true;
+                err.textContent = "Stack is full";
+                err.style.display = 'flex';
+                setTimeout(() => {
+                    err.style.display = 'none';
+                }, 1000);
+                errorFullDisplayed = false;
+            }
         }
+
     }
-        
 }
 
-function sPop(){
-    if(stack.length > 0){
-        document.getElementById('stack').removeChild(document.getElementById(stack.length));
+function stackPop() {
+    if (stack.length > 0) {
+        
+        /*document.getElementById('stack').removeChild(document.getElementById(stack.length));
         stack.pop();
-        if(stack.length > 0){
+        if (stack.length > 0) {
             document.getElementById(`${stack.length}`).classList.remove("hideBefore");
             document.getElementById(`${stack.length}`).classList.remove("hideAfter");
+        }*/
+        var elem = document.getElementById(stack.length)
+        console.log(elem)
+        gsap.to(elem, {
+            y: -500,
+            duration: 0.5,
+            ease: "power2.out",
+        });
+        gsap.to(greenArrow, {
+            y: "+=80",
+            duration: 0.25,
+            opacity: 1,
+            ease: "power2.out"
+        });
+        
+        
+        if(stack.length === 1){
+            gsap.to(redArrow, {
+                y: "+=80",
+                duration: 0.25,
+                opacity: 0,
+                ease: "power2.out",
+                onComplete: () => {
+                    animating = false;
+                }
+            });
+        }else{
+            gsap.to(redArrow, {
+                y: "+=80",
+                duration: 0.25,
+                opacity: 1,
+                ease: "power2.out",
+                onComplete: () => {
+                    animating = false;
+                }
+            });
         }
-           
-    }     
-    else{
-        if(errorEmptyDisplayed === false && errorFullDisplayed === false){
+        stackElem.removeChild(elem);
+        stack.pop();
+
+    }
+    else {
+        if (errorEmptyDisplayed === false && errorFullDisplayed === false) {
             errorEmptyDisplayed = true;
             err.textContent = "Stack is empty";
             err.style.display = 'flex';
             setTimeout(() => {
                 err.style.display = 'none';
-            }, 1000); 
+            }, 1000);
             errorEmptyDisplayed = false;
         }
     }
-}   
+}
 //------------------------------------
 //onchange
-function changeValue(){
+function changeValue() {
     inputval = input.value;
 }
 
